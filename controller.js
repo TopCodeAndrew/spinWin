e54cf46bf2ba4f61f2f332e6bc7c30ac14b81180
Andrew Smith <topcodeandrew@gmail.com>
2022-05-23 08:06:53 -0600

    controller update

--- controller.js
+++ controller.js
@@ -12,34 +12,35 @@ const sequelize = new Sequelize(CONNECTION_STRING, {
     },
 });
 
-let userArr = ["test"];
-
 let todayDate = new Date().toISOString().slice(0, 10);
 
-// console.log(todayDate);
+handleCreateSubmission = async (date, id) => {
+    await sequelize
+        .query(
+            `
+                    INSERT INTO submissions(submission_date, user_id)
+                    VALUES ('${date}', ${id}) RETURNING *`
+        )
+        .then((spinDbRes) => {
+            res.status(200).send(spinDbRes);
+        });
+};
 
 module.exports = {
     submitSpin: async (req, res) => {
         let { iphone_id } = req.body;
-        console.log(iphone_id);
-
         sequelize
             .query(
-                `SELECT * FROM submissions WHERE user_id = ${iphone_id}AND submission_date = '${todayDate}T00:00:00Z'`
+                `SELECT * FROM submissions WHERE user_id = ${iphone_id} AND submission_date = '${todayDate}T00:00:00Z'`
             )
             .then((dbRes) => {
                 if (dbRes[0].length === 0) {
-                    console.log("no record found", dbRes[0]);
-
-                    sequelize
-                        .query(
-                            `
-                    INSERT INTO submissions(submission_date, user_id)
-                    VALUES ('${todayDate}', ${iphone_id}) RETURNING *`
-                        )
-                        .then((spinDbRes) => {
-                            res.status(200).send(spinDbRes);
-                        });
+                    // IF NO SUBMISSION HAS BEEN MADE FOR THIS DAY, CREATE THE SUBMISSION
+                    handleCreateSubmission(todayDate, iphone_id).catch(
+                        (err) => {
+                            console.log(err);
+                        }
+                    );
                 } else {
                     console.log("already queried today", dbRes[0]);
                     res.status(400).send("you already queried today");
@@ -55,18 +56,13 @@ module.exports = {
         //     res.status(200).send("here is a payment");
         // }
     },
-    addUserEmail: async (req, res) => {
-        console.log(req.body);
-        userArr.push(req.body);
-        console.log(userArr);
-        res.status(200).send("successfully added user email");
-    },
     seed: (req, res) => {
         sequelize
             .query(
                 `CREATE TABLE users (
 	user_id serial PRIMARY KEY NOT NULL,
-	iphone_id integer NOT NULL UNIQUE
+	iphone_id integer NOT NULL UNIQUE,
+  user_paypal_email varchar(100) NOT NULL UNIQUE
 );
 
 CREATE TABLE submissions (
@@ -75,8 +71,8 @@ CREATE TABLE submissions (
 	submission_date DATE NOT NULL
 );
 
-INSERT INTO users(iphone_id)
-VALUES (1);
+INSERT INTO users(iphone_id, user_paypal_email)
+VALUES (1, 'tester@test.com');
 
 INSERT INTO submissions(submission_date, user_id)
 VALUES ('2000-12-31', 1);`
